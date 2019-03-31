c03948f85a45f0da3937a45583c53c6b751be3f3
etienne-lelouet <etiennelelouet@outlook.com>
2019-03-31 17:27:45 +0200

    initial commit

---
+++ slipWeather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  slipWeather
+//
+//  Created by Etienne Le Louet on 31/03/2019.
+//  Copyright © 2019 slipSoft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
